d246f4b87a1a0a177ace0dd1459ce69d57d47779
Moschifire <markoladeinde@gmail.com>
2023-06-26 23:52:31 -0700

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class square."""
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+        size (int): The size or the new sqaure.
+        """
+        self.__size = size
