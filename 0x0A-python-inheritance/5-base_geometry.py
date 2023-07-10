8b46a3de6fd38b10fe21b7136351bd4d1ee77d5d
Moschifire <markoladeinde@gmail.com>
2023-07-10 01:55:44 -0700

    Added 5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Mark Oladeinde <markoladeinde@gmail.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
